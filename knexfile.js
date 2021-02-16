15c7e8fa9789554a03731af2fd14893e9c09f251
Kelgors <matthieuboheas@pm.me>
2021-02-16 19:08:59 +0100

    knex config

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,12 @@
 require('./environment');
 module.exports = {
   client: 'postgresql',
-  connection: process.env.DATABASE_URL,
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: true
+    }
+  },
   pool: {
     min: Number(process.env.DATABASE_POOL_MIN) || 1,
     max: Number(process.env.DATABASE_POOL_MAX) || 5
