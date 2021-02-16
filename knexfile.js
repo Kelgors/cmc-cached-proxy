537196d717f2053cbbbe9b34b645f9db4ba376e6
Kelgors <matthieuboheas@pm.me>
2021-02-16 19:01:48 +0100

    add ssl

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 require('./environment');
 module.exports = {
   client: 'postgresql',
-  connection: process.env.DATABASE_URL + '?ssl=true',
+  connection: process.env.DATABASE_URL,
   pool: {
     min: Number(process.env.DATABASE_POOL_MIN) || 1,
     max: Number(process.env.DATABASE_POOL_MAX) || 5
